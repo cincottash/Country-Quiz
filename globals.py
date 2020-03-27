102caef1e579c1bd16ff553bf76ba9d5663dd535
cincottash <cincotta@udel.edu>
2020-03-27 02:44:33 -0400

    almost there

---
+++ globals.py
@@ -0,0 +1,9 @@
+canvasWidth = 1920
+canvasHeight = 1080
+
+textPos = (850, 919)
+
+colors = {
+	"BLACK": (0, 0, 0),
+	"RED": (255, 0, 0)
+}
--- main.py
+++ main.py
@@ -1,31 +1,73 @@
 import pygame
+from globals import *
+from answerKey import *
+from countries import *
+import time
 
 pygame.init()
-clock = pygame.time.Clock()
 
-canvasWidth = 1920
-canvasHeight = 1080
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
 #used to reset the screen after each update
 background = pygame.image.load("assets/world.png")
 
 canvas = pygame.display.set_mode((canvasWidth, canvasHeight))
 
-def update():
-	canvas.blit(background, (0,0))
-	pygame.display.update()
+def clearText():
+	pygame.draw.rect(canvas, (182, 220, 243), (850, 919, 600, 30))
 
-	#Checking if the mouse was pressed
-	for event in pygame.event.get():
-		if event.type == pygame.MOUSEBUTTONUP:
-			#getting pos of the mouse when its clicked
-			pos = pygame.mouse.get_pos()
-			#find the pixel color value at that location
-			color = canvas.get_at(pos)
-			print(color)
+def displayText(text):
+	clearText()
+	myfont = pygame.font.SysFont('Comic Sans MS', 30)
+	textsurface = myfont.render(text, False, colors["BLACK"])
+	canvas.blit(textsurface, textPos)
+	pygame.display.update()
+	time.sleep(1)
+
+def gameLoop():
+	canvas.blit(background, (0,0))
+	#skip button
+	pygame.draw.rect(canvas, colors["RED"],(895,919,50,60))
+	myfont = pygame.font.SysFont('Comic Sans MS', 30)
+	textsurface = myfont.render("Skip", False, colors["BLACK"])
+	canvas.blit(textsurface, (899, 953))
+	pygame.display.update()
+	
+
+
+	for i,country in enumerate(worldCountries):
+		
+		displayText("Click on {}".format(worldCountries[i]))
+
+		validInput = False
+		while(validInput == False):
+			#run until pressed skip or guessed the correct country
+			for event in pygame.event.get():
+				if event.type == pygame.MOUSEBUTTONUP:
+					#getting pos of the mouse when its clicked
+					pos = pygame.mouse.get_pos()
+					#print(pos)
+					
+					#First check if we pressed the skip button
+					if((895 < pos[0] < 944) and (949 < pos[1] < 978)):
+						displayText("pressed skip")
+						#validInput = True
+						validInput = True
+					else:
+						#find the pixel color value at that location
+						color = canvas.get_at(pos)
+						#print(color)
+
+						if(color == key[country]):
+							displayText("Correct!")
+							validInput = True
+						else:
+							displayText("Wrong!")
+							displayText("Try again, click on {}".format(worldCountries[i]))
+	exit(0)
+
 def main():
-	while True:
-		update()
+	gameLoop()
 
 if __name__ == '__main__':
 	main()
